8dbbf797bb1fbc856c064a73fa749bcef2f773e4
Sergio do Nascimento Pereira <ssergionp@gmail.com>
2026-07-27 16:46:31 -0300

    chore: setup inicial do projeto Spring Boot

---
+++ src/test/java/com/ssergionp/taskmanagerapi/TaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssergionp.taskmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
